31100177322dcaf7a662b38744e6226ec0ab883f
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-04 23:51:26 -0400

    specify full path to Python 2.7

--- run/addr_parse.R
+++ run/addr_parse.R
@@ -1,8 +1,8 @@
 # function that returns parsed address from an address string using a python script
 
 addr_parse <- function(address.string,return.call=FALSE) {
-  out.json <- system2('python',c('./run/addr_parse.py',
-                                 shQuote(address.string)),stdout=TRUE)
+  out.json <- system2('/Library/Frameworks/Python.framework/Versions/2.7/bin/python',
+                      c('run/addr_parse.py',shQuote(address.string)),stdout=TRUE)
   out.list <- jsonlite::fromJSON(out.json,flatten=FALSE)
   out.address <- out.list[[1]]
   out.type <- out.list[[2]]
@@ -14,3 +14,5 @@ addr_parse <- function(address.string,return.call=FALSE) {
 ## ex
 # addr_parse('4101 Spring Grove Ave Cincinnati OH 45223')
 # addr_parse('737 US 50 Cincinnati OH 45150')
+# addr_parse('9100 I-275 NB TO I-74 EB RAMP Cincinnati OH 45002')
+
