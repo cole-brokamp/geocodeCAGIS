4c575c98a0b07b5cd1e3da8c070630828bf250b5
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-04 23:33:58 -0400

    add addr_parse functions

---
+++ run/addr_parse.R
@@ -0,0 +1,16 @@
+# function that returns parsed address from an address string using a python script
+
+addr_parse <- function(address.string,return.call=FALSE) {
+  out.json <- system2('python',c('./run/addr_parse.py',
+                                 shQuote(address.string)),stdout=TRUE)
+  out.list <- jsonlite::fromJSON(out.json,flatten=FALSE)
+  out.address <- out.list[[1]]
+  out.type <- out.list[[2]]
+  out.df <- data.frame(as.data.frame(out.address),'type'=out.type)
+  if (return.call) out.df$call <- address.string
+  return(out.df)
+}
+
+## ex
+# addr_parse('4101 Spring Grove Ave Cincinnati OH 45223')
+# addr_parse('737 US 50 Cincinnati OH 45150')
