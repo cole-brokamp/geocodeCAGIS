05ada63da7fba996784aadcb108fc05bed8c7913
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-15 15:58:34 -0400

    save as RData file

--- build_databases/make_CAGIS_database.R
+++ build_databases/make_CAGIS_database.R
@@ -12,11 +12,7 @@ d <- d[!grepl('I-',x=d$address), ]
 source('run/addr_parse.R')
 d.parse <- CB::CBapply(d$address,addr_parse,fill=TRUE,num.cores=4)
 
-d.parsed <- cbind(d,d.parse)
-write.csv(d.parsed,'CAGIS/CAGIS_addrmastr_parsed.csv',row.names=F)
+CAGIS.parsed <- cbind(d,d.parse)
 
-
-# read in CSV and save as RData file
-d.parsed <- read.csv('CAGIS/CAGIS_addrmastr_parsed.csv',stringsAsFactors=FALSE)
-save(d.parsed,file='CAGIS/CAGIS_addrmastr_parsed.RData')
+save(CAGIS.parsed,file='CAGIS/CAGIS_addrmastr_parsed.RData')
 
